b4031befde4f54270fdf7f0a535f02244c941b9a
David <david.scheu97@gmail.com>
2026-09-18 17:01:35 +0200

    initial commit

---
+++ 01_Counter/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Step_A from "./components/step_A.vue";
+</script>
+
+<template>
+
+  <Step_A>
+
+  </Step_A>
+
+
+</template>
+
---
+++ 01_Counter/src/components/step_A.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+function add() {
+  counter.value++
+}
+
+function substract() {
+  counter.value--
+}
+
+</script>
+
+<template>
+
+  <h1>Counter: {{counter}}</h1>
+
+  <button id="add" @click="add">
+    + add
+  </button>
+
+  <button id="substract" @click="substract">
+    - substract
+  </button>
+
+
+</template>
